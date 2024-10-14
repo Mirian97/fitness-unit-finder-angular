7bb3f35418a81a84345131f38d9a3cc6a27ecd53
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-10-13 22:18:08 -0300

    feat: config app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LegendComponent } from './components/legend/legend.component';
+import { FindUnitsService } from './services/find-units.service';
+import { Location } from './types/location.type';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FormsComponent,
+    CardListComponent,
+    CommonModule,
+    LegendComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'smartfit-challenge';
+  showList = new BehaviorSubject(false);
+  unitList: Location[] = [];
+
+  constructor(private unitService: FindUnitsService) {}
+
+  onSubmit() {
+    this.unitList = this.unitService.getFilteredUnits();
+    this.showList.next(true);
+  }
 }
